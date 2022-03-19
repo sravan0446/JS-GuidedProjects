a6ae84c01ad2e8d8a4f94632c53204dcdd0d3451
sravan <shivasravan5799@gmail.com>
2022-03-19 06:39:41 +0530

    added event listner to delete element

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-var book=document.querySelector("#book-list")
-console.log(book.childNodes)
+var dbutton=document.querySelectorAll("#adding-book .delete")
+Array.from(dbutton).forEach(function(btn){
+btn.addEventListener('click',function(e){
+    const li=e.target.parentElement;
+
+    li.parentNode.removeChild(li)
+
+
+})
+})
